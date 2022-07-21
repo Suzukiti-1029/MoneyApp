a30b75e29575639343f97a3605838bfa0d306f28
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-07-21 22:51:01 +0000

    Recordコントローラ作成

---
+++ money/src/main/java/com/example/money/controller/RecordController.java
@@ -0,0 +1,9 @@
+package com.example.money.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/record")
+public class RecordController {
+}
