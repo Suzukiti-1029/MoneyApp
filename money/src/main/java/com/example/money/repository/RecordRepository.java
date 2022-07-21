b940e2e86d9c4cb27c5c3ff9ceefcccc8053e085
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-07-21 23:05:58 +0000

    import文の欠如を修正

--- money/src/main/java/com/example/money/repository/RecordRepository.java
+++ money/src/main/java/com/example/money/repository/RecordRepository.java
@@ -2,5 +2,7 @@ package com.example.money.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.money.model.Record;
+
 public interface RecordRepository extends JpaRepository<Record, Long> {
 }
