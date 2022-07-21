bc1a9cbb987662ec1a147fdf40c4489af53a594d
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-07-21 23:02:30 +0000

    /record/index・/recordのURI設定

--- money/src/main/java/com/example/money/controller/RecordController.java
+++ money/src/main/java/com/example/money/controller/RecordController.java
@@ -1,9 +1,14 @@
 package com.example.money.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/record")
 public class RecordController {
+  @GetMapping({"", "/index"})
+  public String getIndex() {
+    return "record/index";
+  }
 }
