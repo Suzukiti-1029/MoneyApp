d67a04fc74909b833a68e10628b7540ad6e82bfd
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-07-22 00:19:38 +0000

    他URI設定(/record/{add,create,update,edit,delete})

--- money/src/main/java/com/example/money/controller/RecordController.java
+++ money/src/main/java/com/example/money/controller/RecordController.java
@@ -2,6 +2,7 @@ package com.example.money.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,30 @@ public class RecordController {
   @GetMapping({"", "/index"})
   public String getIndex() {
     return "record/index";
+  }
+
+  @GetMapping("/add")
+  public String getAdd() {
+    return "record/add";
+  }
+
+  @PostMapping("/create")
+  public String postCreate() {
+    return "record/create";
+  }
+
+  @GetMapping("/edit")
+  public String getEdit() {
+    return "record/edit";
+  }
+
+  @PostMapping("/update")
+  public String postUpdate() {
+    return "record/update";
+  }
+
+  @PostMapping("/delete")
+  public String postDelete() {
+    return "record/delete";
   }
 }
