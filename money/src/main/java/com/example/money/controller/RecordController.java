057e118759b67eaac196c29fe94ad09a1860054d
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-07-22 03:39:54 +0000

    Create実装(フォームをエンティティとバインド)

--- money/src/main/java/com/example/money/controller/RecordController.java
+++ money/src/main/java/com/example/money/controller/RecordController.java
@@ -3,9 +3,11 @@ package com.example.money.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.money.model.Record;
 import com.example.money.service.RecordService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,7 +25,7 @@ public class RecordController {
   }
 
   @GetMapping("/add")
-  public String getAdd() {
+  public String getAdd(@ModelAttribute Record record) {
     return "record/add";
   }
 
